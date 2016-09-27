7a6fe40a52a02e32585f357b8298ab576e700673
Fabrizio Lazzaretti <fabrizio@lazzaretti.me>
2016-09-27 18:15:08 +0200

    exercise02.2

--- test/exercise01test/FactorialDoubleTest.java
+++ test/exercise01test/FactorialDoubleTest.java
@@ -1,13 +1,22 @@
 package exercise01test;
 import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
 import org.junit.Test;
 
 import exercise01.FactorialDouble;
 
 public class FactorialDoubleTest {
+
+    protected FactorialDouble factorial;
+
+    @Before
+    public void setUp(){
+        factorial = new FactorialDouble();
+    }
+
     @Test
     public void getFactorialTest() throws Exception{
-        FactorialDouble factorial = new FactorialDouble();
         assertEquals( 3840, factorial.getFactorialDouble(10) );
     }
 
---
+++ test/exercise02/FactorialDoubleTest.java
@@ -0,0 +1,9 @@
+package exercise02;
+
+public class FactorialDoubleTest extends exercise01test.FactorialDoubleTest{
+
+    @Override
+    public void setUp() {
+        factorial = new FactorialDouble();
+    }
+}
