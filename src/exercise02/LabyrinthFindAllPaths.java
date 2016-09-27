1745d4a0674ee2d2ed9d2bc56a517a1fceb9ef9e
Fabrizio Lazzaretti <fabrizio@lazzaretti.me>
2016-09-27 19:28:07 +0200

    exercise02.4 + 5

---
+++ src/exercise02/LabyrinthFindAllPaths.java
@@ -0,0 +1,27 @@
+package exercise02;
+
+public class LabyrinthFindAllPaths extends Labyrinth {
+
+
+    public boolean findPath(int x, int y, String path) {
+        path += "-> [" + x + ", " + y + "] ";
+        if (!isAccessible(x, y) || visited[x][y]) {
+            return false;
+        } else if (x == targetX && y == targetY) {
+            System.out.println(path);
+            return true;
+        } else {
+            visited[x][y] = true;
+            boolean left = findPath(x - 1, y, path);
+            boolean right = findPath(x + 1, y, path);
+            boolean up = findPath(x, y - 1, path);
+            boolean down = findPath(x, y + 1, path);
+            visited[x][y] = false;
+            return left || right || up || down;
+        }
+    }
+
+    public static void main(String[] args) {
+        System.out.println(new LabyrinthFindAllPaths().findPath(0, 0, "PATH: "));
+    }
+}
