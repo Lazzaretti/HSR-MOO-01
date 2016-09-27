d645e72509c8543f22ce9e9f7cfbe3e128d6b414
Fabrizio Lazzaretti <fabrizio@lazzaretti.me>
2016-09-27 18:38:28 +0200

    exercise02.3

---
+++ test/exercise02/RecursiveSumTest.java
@@ -0,0 +1,19 @@
+package exercise02;
+
+import exercise01.Factorial;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class RecursiveSumTest {
+    @Test
+    public void mainTest(){
+        assertEquals(4519,RecursiveSum.getSum(RecursiveSum.randomSeries(1000)));
+    }
+
+    @Test
+    public void emptyTest(){
+        assertEquals(0,RecursiveSum.getSum(new int[0]));
+    }
+
+}
